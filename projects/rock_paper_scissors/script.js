474d6a46543aa7e09ad0bc87c418d7240e58442b
mrlsoarez <murilosoliveira202@gmailcom>
2025-10-15 19:19:14 -0300

    [upload general game logic]

--- projects/rock_paper_scissors/script.js
+++ projects/rock_paper_scissors/script.js
@@ -1,60 +1,104 @@
-let humanScore = 0
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
 
 function getComputerChoice() {
     let array = ["ROCK", "PAPER", "SCISSORS"]
     return array[Math.floor(Math.random() * 3)]
 }
 
+
 function getHumanChoice(choice) {
-    while (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") {
-        choice = prompt("Try again! Choose one of the options.: ROCK, PAPER or SCISSORS").toUpperCase()
-    }
-    return choice.toUpperCase()
+    console.log(choice)
 }
 
-
-function playRound(human, computer, round) {
     
+function playRound(human, computer) {
+    
+    round += 1; 
+
     let ONE_BEATS_THE_OTHER = {
     "ROCK": "SCISSORS",
     "SCISSORS": "PAPER",
     "PAPER": "ROCK" 
     }
 
-    console.log("=======", human, computer, "==============")
-
     for (const [key, value] of Object.entries(ONE_BEATS_THE_OTHER)) {
         
         if (computer == key && human == key) {
-            console.log(`Its a tie this ${round}ª round!`)
+            return `Its a tie this ${round}ª round!`
         }
         if (computer == key && human == value ){
-            console.log(`Computer won this ${round}ª round!`)
             computerScore += 1
-            break
+            return `Computer won this ${round}ª round!`
         }
         else if (human == key && computer == value) {
-            console.log(`Human won this ${round}ª round!`) 
             humanScore += 1
-            break
+            return `Player won this ${round}ª round!`
         }
     
     }
 
 }
 
+function getDOMInteractions() {
 
-for (let i = 0; i < 5; i ++) {
+    function renderScore()  {
+        let player_score = document.querySelector(".Player-Score");
+        let computer_score = document.querySelector(".Computer-Score"); 
 
-    let human_choice = getHumanChoice(prompt("What is your choice? Rock, Paper or Scissors?").toUpperCase())
-    let computer_choice = getComputerChoice()
+        player_score.innerHTML = "";
+        computer_score.innerHTML = "";
 
-    playRound(human_choice, computer_choice, i + 1)
+        computer_score.innerHTML = `Computer.: ${parseInt(computerScore)}`;
+        player_score.innerHTML = `Player.: ${parseInt(humanScore)}`;
+
+    }
+
+    function displayMessage(class_name, message) {
+        let display = document.querySelector(class_name)
+        display.innerHTML = "";
+        let p = document.createElement("p");
+        p.textContent = message;
+        display.append(p);
+    }
+
+    function finishGame() {
+        let final_message = "Player won!"; 
+        if (computerScore == 5)  {final_message = "Computer won!";} 
+        displayMessage(".Message", final_message);
+    }
+
+
+    buttons_html = document.querySelectorAll(".Button");
+    buttons = Array.from(buttons_html);
+    renderScore(); 
+
+    buttons_html.forEach((btn) => {
+
+        btn.addEventListener("click", (e) => {
+
+            let player_choice = btn.id;
+            displayMessage(".Player", `Player.: ${player_choice}`);
+              
+            let computer_choice = getComputerChoice();
+            displayMessage(".Computer", `Computer.: ${computer_choice}`);
+
+            let message = playRound(player_choice, computer_choice);
+            displayMessage(".Message", message); 
+
+            renderScore();
+            
+            if (computerScore == 5 || humanScore == 5) {
+               buttons.map((b) => { b.disabled = true; })
+               finishGame(); 
+            }
+        })
+
+
+
+    })
 
 }
 
-console.log("=====================RESULT==================")
-if (computerScore > humanScore) { console.log (`Computer won with ${computerScore} points!`)}
-else if (humanScore > computerScore) { console.log(`Human won with ${humanScore} points!`)}
-else { console.log("It's a tie!") }
+getDOMInteractions()
