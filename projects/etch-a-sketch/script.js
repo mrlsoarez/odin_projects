cde923fbf7736337ffa4bfa715a4f9e5ca7af058
mrlsoarez <murilosoliveira202@gmailcom>
2025-10-26 14:02:58 -0300

    [add slider]

--- projects/etch-a-sketch/script.js
+++ projects/etch-a-sketch/script.js
@@ -16,12 +16,15 @@ class ColorPicker {
 
 function main() {
 
-    picker = new ColorPicker("rgb(255, 0, 0)", 5); 
-
+    let picker = new ColorPicker("rgb(255, 0, 0)", 5); 
+    
     const picked = document.querySelector(".Picked")
+    const input = document.querySelector("input");
     const GRID = document.querySelector(".Grid");
     
     fillGrid(GRID, picker.size);
+
+
     updatePickedColor(picked, picker.actual_color);
 
     const colors = document.querySelectorAll(".Color.Square"); 
@@ -38,23 +41,39 @@ function main() {
     rows.forEach((row) => row.addEventListener("mousedown", () => {
         row.style.background = picker.actual_color;
     }))
+
+
+    input.addEventListener("input", (e) => {
+        picker.size = input.value;
+        fillGrid(GRID, picker.size, picker.actual_color);
+    })
     
 }
 
 function updatePickedColor(div, color) {
     div.style.background = color;
 }
-function fillGrid(grid, number_per_row) {
+
+function fillGrid(grid, number_per_row, color) {
+
+    grid.innerHTML = "";
 
     number_of_columns = number_per_row ** 2;
 
     grid.style.gridTemplateColumns = `repeat(${number_per_row}, 1fr)`
+    
     
     for (let i = 0; i < number_of_columns; i++) {
         let div = document.createElement("div"); 
         div.className = "Row";
         grid.appendChild(div);
     }
+
+    rows = document.querySelectorAll(".Row")
+
+    rows.forEach((row) => row.addEventListener("mousedown", () => {
+        row.style.background = color;
+    }))
 }
 
 main();
