8eb8ceec5db3c23ad52437bc705400397813977e
mrlsoarez <murilosoliveira202@gmailcom>
2025-11-02 19:03:17 -0300

    [upload slider ui]

--- projects/etch-a-sketch/script.js
+++ projects/etch-a-sketch/script.js
@@ -10,6 +10,10 @@ class ColorPicker {
         const style = window.getComputedStyle(elem, null);
         return style["background-color"];
     }
+
+    getColor() {
+        return this.actual_color
+    }
 }
 
 
@@ -22,9 +26,7 @@ function main() {
     const input = document.querySelector("input");
     const GRID = document.querySelector(".Grid");
     
-    fillGrid(GRID, picker.size);
-
-
+    fillGrid(GRID, picker);
     updatePickedColor(picked, picker.actual_color);
 
     const colors = document.querySelectorAll(".Color.Square"); 
@@ -33,35 +35,41 @@ function main() {
         btn.addEventListener("click", () => {
             picker.actual_color = picker.getCSSProperty(btn);
             updatePickedColor(picked, picker.actual_color);
+            fillGrid(GRID, picker);
         })
     })
-    
-    rows = document.querySelectorAll(".Row")
 
-    rows.forEach((row) => row.addEventListener("mousedown", () => {
-        row.style.background = picker.actual_color;
-    }))
-
+    paintGrid(picker.actual_color)
 
     input.addEventListener("input", (e) => {
         picker.size = input.value;
-        fillGrid(GRID, picker.size, picker.actual_color);
+        fillGrid(GRID, picker);
     })
     
+}
+
+function paintGrid(color)  {
+
+    rows = document.querySelectorAll(".Row");
+
+
+    rows.forEach((row) => row.addEventListener("mouseover", () => {
+        console.log(color);
+        row.style.background = color;
+    }))
 }
 
 function updatePickedColor(div, color) {
     div.style.background = color;
 }
 
-function fillGrid(grid, number_per_row, color) {
+function fillGrid(grid, picker) {
 
     grid.innerHTML = "";
 
-    number_of_columns = number_per_row ** 2;
+    number_of_columns = picker.size ** 2;
 
-    grid.style.gridTemplateColumns = `repeat(${number_per_row}, 1fr)`
-    
+    grid.style.gridTemplateColumns = `repeat(${picker.size}, 1fr)`
     
     for (let i = 0; i < number_of_columns; i++) {
         let div = document.createElement("div"); 
@@ -69,11 +77,9 @@ function fillGrid(grid, number_per_row, color) {
         grid.appendChild(div);
     }
 
-    rows = document.querySelectorAll(".Row")
+ 
+    paintGrid(picker.actual_color);
 
-    rows.forEach((row) => row.addEventListener("mousedown", () => {
-        row.style.background = color;
-    }))
 }
 
 main();
