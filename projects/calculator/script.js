2e710cf7d28fa7872ef87168bf8566ea442c7de9
mrlsoarez <murilosoliveira202@gmailcom>
2025-12-07 20:19:06 -0300

    [fixed logic for visor showing and calculating logic]

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -35,42 +35,83 @@ function clear_history() {
 let button = document.querySelectorAll("button");
 let calculator = new Calculator;
 
-let value_a = null; 
-let value_b = null;
+let value_a = NaN; 
+let value_b = NaN;
 let display = "";
 let operator = "";
+
+let operator_visor_flag = false;
+let first_operation = false; 
+
 
 button.forEach((btn) => {
     btn.addEventListener("click", () => {
 
         if (btn.className == "Number") {
+            
+            if (first_operation) {
+                clear_history();
+                let symbol = "";
+
+                if (operator == "sum") {symbol = "+"}
+                else if (operator == "sub") {symbol = "-"}
+                else if (operator == "mult") {symbol = "*"}
+                else if (operator == "div") {symbol = "/"}
+              
+                update_history(`${value_a} ${symbol} `);
+                first_operation = false;
+            }
+
             display += btn.id; 
             update_history(btn.id);
         }
         
         if (btn.className == "Operator") {
-            
-            if (operator.id != "equal") {
-                operator = btn.id; 
-                update_history(btn.innerHTML);
+
+            if (btn.id != "equal" && first_operation) {
+
+                first_operation = false;
+                clear_history();
+                operator_visor_flag = false;
+
+                update_history(`${value_a}`)
+                
             }
 
-            if (value_a == null) {
+            if (btn.id == "clear") {
+                clear_history();
+                value_a = NaN;
+                value_b =  NaN;
+                operator = "";
+            }
+
+            if (btn.id != "equal" && operator_visor_flag == false) {
+                operator = btn.id; 
+            }
+
+            if (isNaN(value_a)) {
                 value_a = parseFloat(display); 
             }
             else {
                 value_b = parseFloat(display); 
             }
-        
+                    
+            if (operator_visor_flag == false && !(isNaN(value_a))) {
+                update_history(` ${btn.innerHTML} `);
+                operator_visor_flag = true;
+            }
+
             display = "";
 
-            if (value_a != null && value_b != null) {
+            if (!(isNaN(value_a)) && !(isNaN(value_b))) {
          
-                let result = calculator.calculate(btn.id, value_a, value_b);
+                let result = calculator.calculate(operator, value_a, value_b);
+                update_history(" = " + result);
 
-                update_history("=" + result);
                 value_a = result; 
-                value_b = null; 
+                value_b = NaN; 
+
+                first_operation = true;
                 
             }
             
